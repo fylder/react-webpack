509e598a1aa34b96652f389295ebab00d837edc9
fylder <fylder@163.com>
2019-06-03 16:37:09 +0800

    add chartist

---
+++ src/model/charts.jsx
@@ -0,0 +1,150 @@
+import Chartist from 'chartist'
+
+const delays = 80
+const durations = 500
+
+export const dailySalesChart = {
+    data: {
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        series: [[12, 17, 7, 17, 23, 18, 38], [20, 17, 17, 7, 3, 28, 18]]
+    },
+    options: {
+        lineSmooth: Chartist.Interpolation.cardinal({
+            tension: 0
+        }),
+        low: 0,
+        high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+        chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        }
+    },
+    // for animation
+    animation: {
+        draw: data => {
+            if (data.type === 'line' || data.type === 'area') {
+                data.element.animate({
+                    d: {
+                        begin: 600,
+                        dur: 700,
+                        from: data.path
+                            .clone()
+                            .scale(1, 0)
+                            .translate(0, data.chartRect.height())
+                            .stringify(),
+                        to: data.path.clone().stringify(),
+                        easing: Chartist.Svg.Easing.easeOutQuint
+                    }
+                })
+            } else if (data.type === 'point') {
+                data.element.animate({
+                    opacity: {
+                        begin: (data.index + 1) * delays,
+                        dur: durations,
+                        from: 0,
+                        to: 1,
+                        easing: 'ease'
+                    }
+                })
+            }
+        }
+    }
+}
+
+export const lineChart = {
+    lineChartData: {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8],
+        series: [[5, 9, 7, 8, 5, 3, 5, 4]]
+    },
+    lineChartOptions: {
+        low: 0,
+        showArea: true
+    },
+    // for animation
+    animation: {
+        draw: data => {
+            if (data.type === 'line' || data.type === 'area') {
+                data.element.animate({
+                    d: {
+                        begin: 600,
+                        dur: 700,
+                        from: data.path
+                            .clone()
+                            .scale(1, 0)
+                            .translate(0, data.chartRect.height())
+                            .stringify(),
+                        to: data.path.clone().stringify(),
+                        easing: Chartist.Svg.Easing.easeOutQuint
+                    }
+                })
+            } else if (data.type === 'point') {
+                data.element.animate({
+                    opacity: {
+                        begin: (data.index + 1) * delays,
+                        dur: durations,
+                        from: 0,
+                        to: 1,
+                        easing: 'ease'
+                    }
+                })
+            }
+        }
+    }
+}
+
+export const testChart = {
+    testData: {
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        series: [[12, 9, 7, 8, 5], [2, 1, 3.5, 7, 3], [1, 3, 4, 5, 6]]
+    },
+    options: {
+        lineSmooth: Chartist.Interpolation.cardinal({
+            tension: 0
+        }),
+        low: 0,
+        fullWidth: true,
+        // high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+        chartPadding: 10
+    }
+}
+
+export const testChart2 = {
+    testData: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        series: [[1, 5, 2, 5, 4, 3], [2, 3, 4, 8, 1, 2], [5, 4, 3, 2, 1, 0.5]]
+    },
+    options: {
+        low: 0,
+        fullWidth: true,
+        showArea: true,
+        showPoint: false,
+        // high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+        chartPadding: 10
+    },
+    animation: {
+        draw: data => {
+            if (data.type === 'line' || data.type === 'area') {
+                data.element.animate({
+                    d: {
+                        begin: 400 * data.index,
+                        dur: 2000,
+                        from: data.path
+                            .clone()
+                            .scale(1, 0)
+                            .translate(0, data.chartRect.height())
+                            .stringify(),
+                        to: data.path.clone().stringify(),
+                        easing: Chartist.Svg.Easing.easeOutQuint
+                    }
+                })
+            }
+        }
+    }
+}
+
+// module.exports = {
+//     dailySalesChart,
+//     lineChart
+// }
--- src/pagers/Statistics.jsx
+++ src/pagers/Statistics.jsx
@@ -3,7 +3,10 @@ import { withStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 import pink from '@material-ui/core/colors/pink'
+import ChartistGraph from 'react-chartist'
+import { dailySalesChart, lineChart, testChart, testChart2 } from '../model/charts'
 
 const styles = {
     headerCard: {
@@ -16,6 +19,9 @@ const styles = {
         position: 'relative',
         top: 24,
         left: 12
+    },
+    title: {
+        padding: 8
     },
     pager: {
         paddingLeft: 24,
@@ -34,6 +40,7 @@ class Statistics extends React.Component {
 
     render() {
         const { classes } = this.props
+
         return (
             <Container fixed>
                 <Paper elevation={6} className={classes.headerCard}>
@@ -42,7 +49,55 @@ class Statistics extends React.Component {
                     </Typography>
                 </Paper>
                 <Paper className={classes.pager}>
-                    <span>fylder</span>
+                    <Typography variant='h6' color='textPrimary' className={classes.title}>
+                        fylder
+                    </Typography>
+                    <Grid container>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ChartistGraph
+                                className='ct-chart'
+                                data={dailySalesChart.data}
+                                type='Line'
+                                options={dailySalesChart.options}
+                                listener={dailySalesChart.animation}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ChartistGraph
+                                data={lineChart.lineChartData}
+                                options={lineChart.lineChartOptions}
+                                type='Line'
+                                listener={dailySalesChart.animation}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ChartistGraph
+                                className='ct-chart'
+                                data={dailySalesChart.data}
+                                type='Line'
+                                options={dailySalesChart.options}
+                                listener={dailySalesChart.animation}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ChartistGraph
+                                className='ct-chart'
+                                data={testChart.testData}
+                                type='Line'
+                                options={testChart.options}
+                                // listener={testChart.animation}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <ChartistGraph
+                                className='ct-chart'
+                                data={testChart2.testData}
+                                type='Line'
+                                options={testChart2.options}
+                                listener={testChart2.animation}
+                            />
+                        </Grid>
+                    </Grid>
                 </Paper>
             </Container>
         )
